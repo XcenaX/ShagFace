4019949e04da053bd06975c1720d12cb6427919c
XcenaX <vlad-057@mail.ru>
2020-06-27 20:02:49 +0600

    change requirements

--- main/Classes/Camera.py
+++ main/Classes/Camera.py
@@ -5,12 +5,28 @@ import threading
 
 class Camera(object):
     def __init__(self):
-        self.video = cv2.VideoCapture(0)
+        indexes = self.getCameraIndexes()
+        print("INDEXES: ")
+        print(indexes)
+        self.video = cv2.VideoCapture(indexes[0])
         (self.grabbed, self.frame) = self.video.read()
         threading.Thread(target=self.update, args=()).start()
 
     def __del__(self):
         self.video.release()
+
+    def getCameraIndexes(self):
+        index = 0
+        arr = []
+        i = 10
+        while i > 0:
+            cap = cv2.VideoCapture(index)
+            if cap.read()[0]:
+                arr.append(index)
+                cap.release()
+            index += 1
+            i -= 1
+        return arr
 
     def get_frame(self):
         image = self.frame
